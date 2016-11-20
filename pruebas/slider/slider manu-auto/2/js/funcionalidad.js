9d5a242d394eaf990888a2a7e60e27f2b40d5ce1
pozzo9000 <pozzo9000@gmail.com>
2016-11-20 18:24:08 -0300

    5
    Slider

---
+++ pruebas/slider/slider manu-auto/2/js/funcionalidad.js
@@ -0,0 +1,13 @@
+var slider = $("#slider");
+var botonSiguiente = $("#btn-next");
+var botonAnterior = $("#btn-prev");
+
+$("#slider section:last").insertBefore("#slider section:first");
+
+slider.css("margin-left","-"+100+"%")
+
+function moverD(){
+};
+function(){siguiente.on("click", function(){
+	moverD();
+})
