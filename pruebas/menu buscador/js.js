74914e9777220cf61a27f6bac598784d9e31295f
pozzo9000 <pozzo9000@gmail.com>
2016-11-26 16:28:24 -0300

    1 Juan 26-11-16

--- pruebas/menu buscador/js.js
+++ pruebas/menu buscador/js.js
@@ -1,23 +1,26 @@
 function apt (){
-			$('#myselect').find('option').each(function(){
-    			console.log($(this).text());
-    			console.log($(this).val());
-})
+			$("#popo").on("click", function(){
+    			var aptitud = $("#aptos").val();
+      			console.log(aptitud);
+      			if (ambientes==ambientes){
+					$(".propiedad").hide();
+					$("."+aptitud).fadeIn();}
+			})
 };
 
 function ambientes(){
 			$("#popo").on("click", function(){
 				var ambientes = $("#ambients").val();
 				if (ambientes==ambientes){
-					$(".propiedad").hide()
-					$("."+ambientes).fadeIn()
+					$(".propiedad").hide();
+					$("."+ambientes).fadeIn();
 					console.log(ambientes);
 }})
 };
 
 $( function (){
 	var todo = $("#caca");
-	$(".propiedad").hide();
-	console.log(ambientes);
+	$(".propiedad").hide(0);
 	ambientes();
+	apt();
 });
