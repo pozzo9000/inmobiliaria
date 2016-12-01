09e7b6ebe0d3fd6e6fca85ae5b5a84f9ae820212
pozzo9000 <pozzo9000@gmail.com>
2016-11-30 23:16:47 -0300

    1 30-11 Juan
    Trabajo sobre el buscador de propiedades

--- pruebas/menu buscador/js.js
+++ pruebas/menu buscador/js.js
@@ -1,19 +1,11 @@
-function apt (){
-			$("#popo").on("click", function(){
-    			var aptitud = $("#aptos").val();
-      			console.log(aptitud);
-      			if (ambientes==ambientes){
-					$(".propiedad").hide();
-					$("."+aptitud).fadeIn();}
-			})
-};
-
 function ambientes(){
 			$("#popo").on("click", function(){
+				var aptitud = $("#aptos").val();
 				var ambientes = $("#ambients").val();
-				if (ambientes==ambientes){
+				var tipo = $("#tipo").val();
+				if (ambientes==ambientes && aptitud==aptitud && tipo==tipo){
 					$(".propiedad").hide();
-					$("."+ambientes).fadeIn();
+					$("."+ambientes+"."+aptitud+"."+tipo).fadeIn();
 					console.log(ambientes);
 }})
 };
@@ -22,5 +14,5 @@ $( function (){
 	var todo = $("#caca");
 	$(".propiedad").hide(0);
 	ambientes();
-	apt();
+	//apt();
 });
---
+++ pruebas/menu buscador/jsprolijo.js
@@ -0,0 +1,7 @@
+var buscar = {
+	apt: $("#aptos").val(),
+	acc :function(){
+		console.log("Hola")
+	},
+	search : $("#popo").on("click", acc())
+}
