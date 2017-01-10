b5d3feecf277df6845d47cd1527cc335378f1c92
Juan M. Pozzo Work <Juan M. Pozzo Work>
2017-01-10 01:12:10 -0300

    Siguiendo con la validación del formulario(Ya casi terminando)

--- Final/c/js/val.js
+++ Final/c/js/val.js
@@ -7,7 +7,7 @@ var capture ={
 	valorizar () {
 		this.inNombre = $("#nombre").val();
 		this.inApellido = $("#apellido").val();
-		this.inTelefono = $("#el").val();
+		this.inTelefono = $("#tel").val();
 		this.inMail = $("#mail").val();
 		this.inText = $("#texto").val();
 	},
@@ -34,33 +34,103 @@ var primerNivel = {
 			this.statusNombre = 0400
 		} else if(objNombre.val().length >= 15) {
 			objNombre.addClass("largo");
-			pNombre.addClass("pvacio").hide();
+			pNombre.addClass("plargo").hide();
 			this.statusNombre = 0400
 		} else if(!expresionNombre.test(capture.inNombre)) {
 			objNombre.addClass("inex");
 			pNombre.addClass("pinex").hide();
 			this.statusNombre = 0400
 		}else {
-			pNombre.hide();
+			objNombre.addClass("correct");
 			this.statusNombre = 1345
 		}
 	},
 	funcApellido() {
-		
+		capture.valorizar();
+		var objApellido = $("#apellido");
+		var pApellido = $("#papell");
+		var expresionApellido =/^[A-Za-z\u00C0-\u017F]*$/;
+		if (capture.inApellido==="") {
+			objApellido.addClass("vacio");
+			pApellido.addClass("pvacio").hide();
+			this.statusApellido = 0400
+		} else if(objApellido.val().length <= 3) {
+			objApellido.addClass("corto");
+			pApellido.addClass("pcorto").hide();
+			this.statusApellido = 0400
+		} else if(objApellido.val().length >= 15) {
+			objApellido.addClass("largo");
+			pApellido.addClass("plargo").hide();
+			this.statusApellido = 0400
+		} else if(!expresionApellido.test(capture.inApellido)) {
+			objApellido.addClass("inex");
+			pApellido.addClass("pinex").hide();
+			this.statusApellido = 0400
+		}else {
+			objApellido.addClass("correct");
+			this.statusApellido = 1345
+		}
 	},
 	funcTelefono() {
-		
+		capture.valorizar();
+		var objTelefono = $("#tel");
+		var pTelefono = $("#ptel");
+		var expresionTel =/^[A-Za-z\u00C0-\u017F]*$/;
+		if (objTelefono.val().length == 0) {
+			objTelefono.addClass("vacio");
+			pTelefono.addClass("pvacio").hide();
+			this.statusTelefono = 0400
+		} else if(objTelefono.val().length <= 7) {
+			objTelefono.addClass("corto");
+			pTelefono.addClass("pcorto").hide();
+			this.statusTelefono = 0400
+		} else if(objTelefono.val().length >= 13) {
+			objTelefono.addClass("largo");
+			pTelefono.addClass("plargo").hide();
+			this.statusTelefono = 0400
+		} else if(isNaN(capture.inTelefono)) {
+			objTelefono.addClass("inex");
+			pTelefono.addClass("pinex").hide();
+			this.statusTelefono = 0400
+		}else {
+			objTelefono.addClass("correct");
+			this.statusTelefono = 1345
+		}
 	},
 	funcMail() {
-		
+		capture.valorizar();
+		var objTelefono = $("#tel");
+		var pTelefono = $("#ptel");
+		var expresionTel =/^[A-Za-z\u00C0-\u017F]*$/;
+		if (objTelefono.val().length == 0) {
+			objTelefono.addClass("vacio");
+			pTelefono.addClass("pvacio").hide();
+			this.statusTelefono = 0400
+		} else if(objTelefono.val().length <= 7) {
+			objTelefono.addClass("corto");
+			pTelefono.addClass("pcorto").hide();
+			this.statusTelefono = 0400
+		} else if(objTelefono.val().length >= 13) {
+			objTelefono.addClass("largo");
+			pTelefono.addClass("plargo").hide();
+			this.statusTelefono = 0400
+		} else if(isNaN(capture.inTelefono)) {
+			objTelefono.addClass("inex");
+			pTelefono.addClass("pinex").hide();
+			this.statusTelefono = 0400
+		}else {
+			objTelefono.addClass("correct");
+			this.statusTelefono = 1345
+		}
 	},
 	funcText() {
 		
 	},
 	funcParrafos() {
 		$(".pvacio").text("Este contenedor está vacio").slideDown(500);
-		$(".pinex").text("Formato de texto erroneo").slideDown(500);
-		$(".pcorto").text("Este campo exije 3 caracteres").slideDown(500);
+		$(".pinex").text("Formato erroneo").slideDown(500);
+		$(".pcorto").text("Este texto es muy corto").slideDown(500);
+		$(".plargo").text("Se exedio el maximo de caracteres").slideDown(500);
 	},
 	funcValidar(){
 		var sectores = $(".sector");
@@ -68,7 +138,7 @@ var primerNivel = {
 		parrafos.slideUp(500, function(){
 			$().empty()
 		});
-		sectores.removeClass("vacio").removeClass("corto").removeClass("largo").removeClass(".inex");
+		sectores.removeClass("vacio").removeClass("corto").removeClass("largo").removeClass(".inex").removeClass(".correct");
 		parrafos.removeClass("pvacio").removeClass("pcorto").removeClass("plargo").removeClass("pinex");
 		this.funcNombre();
 		this.funcApellido();
