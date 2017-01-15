f8c9f48ef6ca46132ce9bca1418a9390744f1122
Juan M. Pozzo Work <Juan M. Pozzo Work>
2017-01-15 18:24:39 -0300

    Hola estoy trabajando re raro

--- Final/t/js/val.js
+++ Final/t/js/val.js
@@ -1,3 +1,9 @@
+var caca ={
+ valorizar() {
+	alert("Que se yo")
+}
+};
+// ------------------------------------------------------------------------------------------------------------
 function checkear() {
 	alert("No hay patos en angola (creo)");
 	return false
