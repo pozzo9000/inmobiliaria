1517c19cc335dd0aac9827a2a55af4e5dd25cde1
Juan M. Pozzo Work <Juan M. Pozzo Work>
2017-01-15 20:26:45 -0300

    Prueba de val de tas

--- Final/t/js/val.js
+++ Final/t/js/val.js
@@ -1,13 +1,40 @@
-var capture = {
+var capturar = {
+	nombre: "este campo está vacio",
+	apellido: "este campo está vacio",
+	tel: "este campo está vacio",
+	telDos: "este campo está vacio",
+	mail: "este campo está vacio",
+	mailDos: "este campo está vacio",
+	nomCapture() {
+		this.nombre = $("#nomb").val()
+	},
+	apellCapture() {
 
+	},
+	telCapture() {
+
+	},
+	telDCapture() {
+
+	},
+	mailCapture() {
+
+	},
+	mailDCapture() {
+
+	}
 };
+var validar = {
+	statusNombre : 0400,
 
-var primerPiso = {
-
-};
-
-var segundoPiso = {
-};
+	valNombre() {
+		capturar.nomCapture();
+		var objNomb = $("#nomb")
+		if (capturar.nombre==="") {
+			
+		}
+	}
+}
 // ------------------------------------------------------------------------------------------------------------
 function checkear() {
 	alert("No hay patos en angola (creo)");
