aa07bfb4dff34bdb36d2d13234bf33f26a554d3c
pozzo9000 <pozzo9000@gmail.com>
2017-03-25 00:51:15 -0300

    retrabajando laa val de contacto

--- Final/c/js/val.js
+++ Final/c/js/val.js
@@ -1,3 +1,11 @@
+$(iniciar);
+function iniciar(){
+	$("#nombre").on('keyup', function(){primerNivel.funcNombre();});
+	$("#nombre").blur('keyup', function(){primerNivel.funcNombre();});
+	$("#apellido").on('keyup', function(){primerNivel.funcApellido();});
+	$("#apellido").blur('keyup', function(){primerNivel.funcApellido();});
+};
+
 var capture ={
 	inNombre : "Este campo está vacio",
 	inApellido : "Este campo está vacio",
@@ -21,127 +29,156 @@ var primerNivel = {
 
 	funcNombre (){
 		capture.valorizar();
+		this.funcStil();
 		var objNombre = $("#nombre");
 		var pNombre = $("#pnomb");
 		var expresionNombre =/^[A-Za-z\u00C0-\u017F]*$/;
 		if (capture.inNombre==="") {
 			objNombre.addClass("vacio");
 			pNombre.addClass("pvacio").hide();
+			this.funcParrafos();
 			this.statusNombre = 0400
 		} else if(objNombre.val().length <= 3) {
 			objNombre.addClass("corto");
 			pNombre.addClass("pcorto").hide();
+			this.funcParrafos();
 			this.statusNombre = 0400
 		} else if(objNombre.val().length >= 15) {
 			objNombre.addClass("largo");
 			pNombre.addClass("plargo").hide();
+			this.funcParrafos();
 			this.statusNombre = 0400
 		} else if(!expresionNombre.test(capture.inNombre)) {
 			objNombre.addClass("inex");
 			pNombre.addClass("pinex").hide();
+			this.funcParrafos();
 			this.statusNombre = 0400
 		}else {
 			objNombre.addClass("correct");
+			this.funcParrafos();
 			this.statusNombre = 1345
 		}
 	},
 	funcApellido() {
 		capture.valorizar();
+		this.funcStil();
 		var objApellido = $("#apellido");
 		var pApellido = $("#papell");
 		var expresionApellido =/^[A-Za-z\u00C0-\u017F]*$/;
 		if (capture.inApellido==="") {
 			objApellido.addClass("vacio");
 			pApellido.addClass("pvacio").hide();
+			this.funcParrafos();
 			this.statusApellido = 0400
 		} else if(objApellido.val().length <= 3) {
 			objApellido.addClass("corto");
 			pApellido.addClass("pcorto").hide();
+			this.funcParrafos();
 			this.statusApellido = 0400
 		} else if(objApellido.val().length >= 15) {
 			objApellido.addClass("largo");
 			pApellido.addClass("plargo").hide();
+			this.funcParrafos();
 			this.statusApellido = 0400
 		} else if(!expresionApellido.test(capture.inApellido)) {
 			objApellido.addClass("inex");
 			pApellido.addClass("pinex").hide();
+			this.funcParrafos();
 			this.statusApellido = 0400
 		}else {
 			objApellido.addClass("correct");
+			this.funcParrafos();
 			this.statusApellido = 1345
 		}
 	},
 	funcTelefono() {
 		capture.valorizar();
+		this.funcStil();
 		var objTelefono = $("#tel");
 		var pTelefono = $("#ptel");
 		var expresionTel =/[0-9\-\+]/;
 		if (objTelefono.val().length == 0) {
 			objTelefono.addClass("vacio");
 			pTelefono.addClass("pvacio").hide();
+			this.funcParrafos();
 			this.statusTelefono = 0400
 		} else if(objTelefono.val().length <= 7) {
 			objTelefono.addClass("corto");
 			pTelefono.addClass("pcorto").hide();
+			this.funcParrafos();
 			this.statusTelefono = 0400
 		} else if(objTelefono.val().length >= 25) {
 			objTelefono.addClass("largo");
 			pTelefono.addClass("plargo").hide();
+			this.funcParrafos();
 			this.statusTelefono = 0400
 		} else if(!expresionTel.test(capture.inTelefono)) {
 			objTelefono.addClass("inex");
 			pTelefono.addClass("pinex").hide();
+			this.funcParrafos();
 			this.statusTelefono = 0400
 		}else {
 			objTelefono.addClass("correct");
+			this.funcParrafos();
 			this.statusTelefono = 1345
 		}
 	},
 	funcMail() {
 		capture.valorizar();
+		this.funcStil();
 		var objMail = $("#mail");
 		var pMail = $("#pmail");
 		var expresionMail =/\w+@\w+\.+[a-z]/;
 		if (capture.inMail == "") {
 			objMail.addClass("vacio");
 			pMail.addClass("pvacio").hide();
+			this.funcParrafos();
 			this.statusMail = 0400
 		} else if(objMail.val().length <= 9) {
 			objMail.addClass("corto");
 			pMail.addClass("pcorto").hide();
+			this.funcParrafos();
 			this.statusMail = 0400
 		} else if(objMail.val().length >= 25) {
 			objMail.addClass("largo");
 			pMail.addClass("plargo").hide();
+			this.funcParrafos();
 			this.statusMail = 0400
 		} else if(!expresionMail.test(capture.inMail)) {
 			objMail.addClass("inex");
 			pMail.addClass("pinex").hide();
+			this.funcParrafos();
 			this.statusMail = 0400
 		}else {
 			objMail.addClass("correct");
+			this.funcParrafos();
 			this.statusMail = 1345
 		}
 	},
 	funcText() {
 		capture.valorizar();
+		this.funcStil();
 		var objText = $("#texto");
 		var pText = $("#parea");
 		var expresionText =/^[A-Za-z0-9\$"\s\-\.\u00C0-\u017F]*$/;
 		if (capture.inText === "") {
 			objText.addClass("vacio");
 			pText.addClass("pvacio").hide();
+			this.funcParrafos();
 			this.statusTexto = 0400
 		} else if(objText.val().length <= 10) {
 			objText.addClass("corto");
 			pText.addClass("pcorto").hide();
+			this.funcParrafos();
 			this.statusTexto = 0400
 		} else if(!expresionText.test(capture.inText)) {
 			objText.addClass("inex");
 			pText.addClass("pinex").hide();
+			this.funcParrafos();
 			this.statusTexto = 0400
 		}else {
 			objText.addClass("correct");
+			this.funcParrafos();
 			this.statusTexto = 1345
 		}
 	},
@@ -151,7 +188,7 @@ var primerNivel = {
 		$(".pcorto").text("•Este texto es muy corto").slideDown(500);
 		$(".plargo").text("•Se exedio el maximo de caracteres").slideDown(500);
 	},
-	funcValidar(){
+	funcStil(){
 		var sectores = $(".sector");
 		var parrafos = $(".pstatus");
 		parrafos.slideUp(500, function(){
@@ -159,6 +196,8 @@ var primerNivel = {
 		});
 		sectores.removeClass("vacio").removeClass("corto").removeClass("largo").removeClass(".inex").removeClass("correct");
 		parrafos.removeClass("pvacio").removeClass("pcorto").removeClass("plargo").removeClass("pinex");
+	},
+	funcValidar() {
 		this.funcNombre();
 		this.funcApellido();
 		this.funcTelefono();
